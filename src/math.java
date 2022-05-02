096e5f8e8d5c0520927ad82048b363cb9e77ee03
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-05-02 23:40:17 +0300

    Initial commit

---
+++ src/math.java
@@ -0,0 +1,17 @@
+public class math {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int c = 9;
+
+        double d = ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
+        float k =(float) ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
+        int f = (int) ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(k);
+
+    }
+
+}
