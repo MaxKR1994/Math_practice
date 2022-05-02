73e475a535acd439aaa962915eecd93745f659a8
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-05-02 23:45:28 +0300

    Expression changed

--- src/math.java
+++ src/math.java
@@ -3,9 +3,9 @@ public class math {
     public static void main(String[] args) {
         int a = 4;
         int b = 2;
-        int c = 9;
+        int c = 4;
 
-        double d = ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
+        double d = ((Math.abs(a+b))/(Math.pow((a+b),3)-Math.sqrt(c)));
         float k =(float) ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
         int f = (int) ((Math.abs(a+b))/Math.pow((a+b),3)-Math.sqrt(c));
         System.out.println(d);
