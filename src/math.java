7459023a997a4b1ca77d53616540b462803b594f
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-05-03 00:51:18 +0300

    Optimization final

--- src/math.java
+++ src/math.java
@@ -1,17 +1,18 @@
 public class math {
 
     public static void main(String[] args) {
-        int a = 2;
-        float b = 4.2f;
-        int c = 9;
+        double a = 2.0f;
+        double b = 4.2f;
+        double c = 9.0f;
         int d = (int) Math.abs(a-b);
-        int g = a + (int) b;
+        int g = (int) a + (int) b;
         double v = Math.pow(g, 3) - Math.sqrt(c);
         int k = (int) (d/ v);
-        double f = d/(v);
-        float j = (float) (d/(v));
+        double f = d/v;
+        float j = (float) (d/v);
         System.out.println(d);
         System.out.println(g);
+        System.out.println(v);
         System.out.println(k);
         System.out.println(f);
         System.out.println(j);
